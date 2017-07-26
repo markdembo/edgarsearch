db35f68d2f2de5ea3876b07084552781bde9941c
markdembo <dembo.mark@gmail.com>
2017-07-26 22:19:55 +0200

    Changed path to release on Github

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-"""Crawl EDGAR database to download index files and filings.
-
-Contains the edgar class.
-
-Sample script:
-my_edgar = edgar()
-my_edgar.definesearch("20150101", "20151231", filter_formtype=["8-K"])
-my_edgar.getindex()
-my_edgar.getfilings(text_only=False, sample_size=10,
-                    fname_form="%Y/%m/%Y%m_%company")
-my_edgar.cur_filings
-
-"""
 from setuptools import setup
 
 setup(name="edgarsearch",
@@ -18,7 +5,7 @@ setup(name="edgarsearch",
       description="Crawl EDGAR database to download index files and filings.",
       keywords="EDGAR index filings 8-K 10-K",
       url="https://github.com/markdembo/edgarsearch",
-      download_url="https://github.com/markdembo/edgarsearch/archive/0.1.tar.gz",
+      download_url="https://github.com/markdembo/edgarsearch/archive/0.2.tar.gz",
       author="Mark Dembo",
       author_email="mark.dembo@student.unisg.ch",
       license="MIT",
